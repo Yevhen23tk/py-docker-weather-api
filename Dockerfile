b7832051acda2964a63231a05adb3feb7c8ca05f
Yevhen Tkachuk <yevhen23tk@gmail.com>
2026-03-23 14:52:15 +0200

    Solution

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.9.9-alpine3.13
+LABEL maintainer="yevhen23tk@gmail.com"
+
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+ENTRYPOINT ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,39 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+BASE_URL = "https://api.weatherapi.com/v1/current.json"
+DEFAULT_CITY = "Paris"
+
+
+def get_weather(city: str = DEFAULT_CITY) -> None:
+    if not API_KEY:
+        raise ValueError("API_KEY must be set")
+
+    url = "https://api.weatherapi.com/v1/current.json"
+
+    params = {"key": API_KEY, "q": city}
+
+    response = requests.get(url, params=params, timeout=10)
+    response.raise_for_status()
+
+    data = response.json()
+
+    city = data["location"]["name"]
+    country = data["location"]["country"]
+    temp_c = data["current"]["temp_c"]
+    humidity = data["current"]["humidity"]
+    condition = data["current"]["condition"]["text"]
+    wind_kph = data["current"]["wind_kph"]
+
+    print(f"Weather in {city}, {country}: ")
+    print(f"Temperature: {temp_c}°C")
+    print(f"Humidity: {humidity}%")
+    print(f"Condition: {condition}")
+    print(f"Wind speed: {wind_kph} kph")
 
 
 if __name__ == "__main__":
